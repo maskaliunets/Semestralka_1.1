71a04277c2fe784188293b8576295a36341d556f
maskaliunets <maskaliunets@gmail.com>
2020-01-09 21:02:57 +0100

    commit hidden maven tests

--- src/test/java/cz/vse/xmass07/rukovoditel/selenium/ProjectsTest.java
+++ src/test/java/cz/vse/xmass07/rukovoditel/selenium/ProjectsTest.java
@@ -16,5 +16,89 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-public class ProjectsTest {
+public class ProjectsTest {/*
+    private ChromeDriver driver;
+
+    private final static String PROJECT_URL = UtilTest.URL + "index.php?module=items/items&path=21";
+
+    @Before
+    public void init() {
+        driver = UtilTest.initChrome();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void projectWithoutNameIsNotCreated() {
+        //Given
+        UtilTest.login(driver, "rukovoditel", "vse456ru");
+        driver.get(PROJECT_URL);
+        //When
+        UtilTest.openCreateModal(driver);
+        WebElement nameInput = driver.findElement(By.id("fields_158"));
+        nameInput.clear();
+        driver.findElement(By.className("btn-primary-modal-action")).click();
+        //Then
+        WebElement errorTextElement = driver.findElement(By.id("fields_158-error"));
+        Assert.assertEquals(errorTextElement.getText(), "This field is required!");
+        WebElement alert = driver.findElement(By.cssSelector(".modal-footer .alert-danger"));
+        Assert.assertEquals(alert.getText(), "Some fields are required. They have been highlighted above.");
+    }
+
+    @Test
+    public void createAndDeleteProject() {
+        //Project with status New, priority High and filled start date as today is created.
+        // Verify that there is new row in project table.
+        // Delete project after test.
+        // Given
+        UtilTest.login(driver, "rukovoditel", "vse456ru");
+        driver.get(PROJECT_URL);
+        //WHEN
+        UtilTest.openCreateModal(driver);
+        //Set value to selects
+        Select prioritySelect = new Select(driver.findElement(By.id("fields_156")));
+        Select statusSelect = new Select(driver.findElement(By.id("fields_157")));
+        prioritySelect.selectByVisibleText("High");
+        statusSelect.selectByVisibleText("New");
+        //Prepare date
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("YYYY-MM-dd");
+        String dateInputText = formatter.format(date);
+        //Fill Project name
+        String projectName = "Standa" + date.getTime();
+        WebElement nameInput = driver.findElement(By.id("fields_158"));
+        nameInput.clear();
+        nameInput.sendKeys(projectName);
+        //Fill Date input
+        WebElement dateInput = driver.findElement(By.id("fields_159"));
+        dateInput.clear();
+        dateInput.sendKeys(dateInputText);
+        //Send form
+        driver.findElement(By.className("btn-primary-modal-action")).click();
+        //Then
+        driver.get(PROJECT_URL);
+        //Reset filter
+        UtilTest.resetFilter(driver);
+        //Check
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#slimScroll > table")));
+        List<WebElement> rows = driver.findElements(By.cssSelector("#slimScroll > table > tbody > tr"));
+        Optional<WebElement> optionalRow = rows.stream().filter(row -> row.findElement(By.cssSelector("td.field-158-td > a")).getText().equals(projectName)).findFirst();
+        Assert.assertTrue(optionalRow.isPresent());
+        WebElement tableRow = optionalRow.get();
+        Assert.assertEquals(tableRow.findElement(By.cssSelector("td.field-158-td > a")).getText(), projectName);
+        //Delete created project
+        deleteProject(tableRow);
+    }
+
+    private void deleteProject(WebElement tableRow) {
+        tableRow.findElement(By.className("fa-trash-o")).click();
+        WebDriverWait modalWait = new WebDriverWait(driver, 3);
+        modalWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("ajax-modal")));
+        driver.findElement(By.id("delete_confirm")).click();
+        driver.findElement(By.className("btn-primary-modal-action")).click();
+    }*/
 }
--- src/test/java/cz/vse/xmass07/rukovoditel/selenium/TaskTest.java
+++ src/test/java/cz/vse/xmass07/rukovoditel/selenium/TaskTest.java
@@ -1,5 +1,6 @@
 package cz.vse.xmass07.rukovoditel.selenium;
 
+
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -16,5 +17,114 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-public class TaskTest {
+public class TaskTest {/*
+
+    private ChromeDriver driver;
+    private final static String PROJECT_DETAIL_URL = UtilTest.URL + "index.php?module=items/items&path=21-509/22";
+    private final static List<String> STATUSES = Arrays.asList("New", "Open", "Waiting", "Done", "Closed", "Paid", "Canceled");
+
+    @Before
+    public void init() {
+        driver = UtilTest.initChrome();
+    }
+
+    @After
+    public void tearDown() {
+        driver.close();
+    }
+
+    @Test
+    public void task1() {
+        //Given
+        UtilTest.login(driver, "rukovoditel", "vse456ru");
+        driver.get(PROJECT_DETAIL_URL);
+        UtilTest.resetFilter(driver);
+        deleteTasks();
+        //When
+        UtilTest.openCreateModal(driver);
+        String taskName = createTask("Task", "New", "Medium");//Set value to selects
+        //Then
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#slimScroll > table")));
+        List<WebElement> rows = driver.findElements(By.cssSelector("#slimScroll > table > tbody > tr"));
+        Optional<WebElement> optionalRow = rows.stream().filter(row -> row.findElement(By.cssSelector("td.field-168-td > a")).getText().equals(taskName)).findFirst();
+        Assert.assertTrue(optionalRow.isPresent());
+        WebElement tableRow = optionalRow.get();
+        tableRow.findElement(By.cssSelector("td.field-163-td > a > i.fa-info")).click();
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.portlet-title > div.caption")).getText(), taskName);
+        Assert.assertEquals(driver.findElement(By.cssSelector("tr.form-group-167 > td")).getText(), "Task");
+        Assert.assertEquals(driver.findElement(By.cssSelector("tr.form-group-169 > td")).getText(), "New");
+        Assert.assertEquals(driver.findElement(By.cssSelector("tr.form-group-170 > td")).getText(), "Medium");
+        Assert.assertEquals(driver.findElement(By.cssSelector(".content_box_content.fieldtype_textarea_wysiwyg")).getText(), "Default description");
+        deleteTasks();
+    }
+
+    @Test
+    public void task2() {
+        //Given
+        UtilTest.login(driver, "rukovoditel", "vse456ru");
+        driver.get(PROJECT_DETAIL_URL);
+        UtilTest.resetFilter(driver);
+        deleteTasks();
+        //When
+        STATUSES.stream().forEach(status -> {
+            UtilTest.openCreateModal(driver);
+            createTask("Task", status, "Medium");
+        });
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        UtilTest.setFilter(driver, Arrays.asList("New", "Open", "Waiting"));//Use Filter 1
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#slimScroll > table")));
+        int result1 = driver.findElements(By.cssSelector("#slimScroll > table > tbody > tr")).size();
+        Assert.assertEquals(result1, 3);//Use Filter 2
+        UtilTest.setFilter(driver, Arrays.asList("New", "Waiting"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#slimScroll > table")));
+        int result2 = driver.findElements(By.cssSelector("#slimScroll > table > tbody > tr")).size();
+        Assert.assertEquals(result2, 2);//Reset Filter
+        UtilTest.resetFilter(driver);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#slimScroll > table")));
+        int result3 = driver.findElements(By.cssSelector("#slimScroll > table > tbody > tr")).size();
+        Assert.assertEquals(result3, 7);
+        deleteTasks();
+    }
+
+    private String createTask(String type, String status, String priority) {
+        Select typeSelect = new Select(driver.findElement(By.id("fields_167")));
+        Select statusSelect = new Select(driver.findElement(By.id("fields_169")));
+        Select priorityStatus = new Select(driver.findElement(By.id("fields_170")));
+        typeSelect.selectByVisibleText(type);
+        statusSelect.selectByVisibleText(status);
+        priorityStatus.selectByVisibleText(priority);
+        Date date = new Date();
+        String taskName = "Task" + date.getTime();
+        WebElement nameInput = driver.findElement(By.id("fields_168"));
+        nameInput.clear();
+        nameInput.sendKeys(taskName);
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.tagName("iframe")));
+        driver.switchTo().frame(driver.findElement(By.tagName("iframe")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cke_contents_ltr")));
+        WebElement descriptionInput = driver.findElement(By.className("cke_contents_ltr"));
+        descriptionInput.clear();
+        descriptionInput.sendKeys("Default description");
+        driver.switchTo().defaultContent();
+        driver.findElement(By.className("btn-primary-modal-action")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#slimScroll > table")));
+        return taskName;
+    }
+
+    public void deleteTasks() {
+        driver.get(PROJECT_DETAIL_URL);
+        WebDriverWait wait = new WebDriverWait(driver, 2);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#slimScroll > table")));
+        if (driver.findElements(By.cssSelector("#slimScroll > table > tbody > tr > td")).size() > 1) {
+            driver.findElement(By.id("select_all_items")).click();
+            driver.findElement(By.cssSelector("[class='btn btn-default dropdown-toggle']")).click();
+            driver.findElement(By.cssSelector("[class='btn btn-default dropdown-toggle']")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Delete")));
+            driver.findElement(By.linkText("Delete")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("btn-primary-modal-action")));
+            driver.findElement(By.className("btn-primary-modal-action")).click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#slimScroll > table")));
+        }
+    }*/
 }
